e3bc09511e8f91844342a0a13e27fbc131c09df8
Jeff Yates <jeff@somewhatabstract.com>
2020-01-21 19:34:43 -0600

    Added implementation of LocalStorageCache
    TODO: Tests

---
+++ packages/wonder-blocks-data/util/local-storage-cache.test.js
@@ -0,0 +1,26 @@
+// @flow
+import LocalStorageCache from "./local-storage-cache.js";
+
+describe("LocalStorageCache", () => {
+    describe("#constructor", () => {
+        it.todo("TODO: Accepts custom storage");
+        it.todo("TODO: Defaults to window.localStorage if exists");
+        it.todo("TODO: Throws if no storage");
+    });
+
+    describe("#store", () => {
+        it.todo("TODO: Deal with QuotaExceeded!");
+    });
+
+    describe("#retrieve", () => {
+        it.todo("TODO!");
+    });
+
+    describe("#remove", () => {
+        it.todo("TODO!");
+    });
+
+    describe("#removeAll", () => {
+        it.todo("TODO!");
+    });
+});
